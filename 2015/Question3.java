0cfdb742e5d0e9ab516cc05b4d152b357ee88b3c
ayushdutta1 <54046142+ayushdutta1@users.noreply.github.com>
2019-09-15 18:16:11 +0530

    Create Question3.java

---
+++ 2015/Question3.java
@@ -0,0 +1,10 @@
+/**
+Write a program to accept a sentence which may be terminated by either ‘.’ or ‘?’ only. 
+The words are to be separated by a single blank space. Print an error message if the input does not terminate with ‘.’ or ‘?’.
+You can assume that no word in the sentence exceeds 15 characters, so that you get a proper formatted output.
+
+Perform the following tasks:
+(i) Convert the first letter of each word to uppercase.
+(ii) Find the number of vowels and consonants in each word and display them with proper headings along with the words.
+*/
+
