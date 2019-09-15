fe0a8d84622aba1cea6419f5615c552da267b7f9
ayushdutta1 <54046142+ayushdutta1@users.noreply.github.com>
2019-09-15 18:32:27 +0530

    Update Question1.java

--- 2014/Question1.java
+++ 2014/Question1.java
@@ -15,3 +15,65 @@ Display the number of Composite magic integers that are in the range between m a
 output them along with the frequency, in the format specified below.
 */
 
+import java.util.*;
+public class Composit_Magic
+{
+intisComposit(int a)
+    {
+inti,f=0,r=0;
+        for(i=1;i<=a;i++)
+        {
+            if(a%i==0)
+            f++;
+        }
+        if(f>2)
+        r=1;
+        return r;
+    }
+intisMagic(int b)
+    {
+intt,r=0,s=0,l;
+        t=b;
+for(;t!=0;)
+        {
+            l=t%10;
+            s=s+l;
+            t=t/10;
+            if((s>9)&&(t==0))
+            {
+                t=s;
+                s=0;
+            }
+        }
+        if(s==1)
+        r=1;
+        return r;
+    }
+    public static void main()
+    {
+        Scanner in=new Scanner(System.in);
+Composit_Magicob=new Composit_Magic();
+intm,n,i,f=0;
+System.out.println("Enter Starting Range : ");
+        m=in.nextInt();
+System.out.println("Enter Ending Range : ");
+        n=in.nextInt();
+        if(m<n)
+        {
+System.out.println("THE COMPOSIT MAGIC INTEGERS ARE : ");
+            for(i=m;i<=n;i++)
+            {
+                if((ob.isComposit(i)==1)&&(ob.isMagic(i)==1))
+                {
+System.out.print(i+" ");
+                    f++;
+                }
+            }
+System.out.println();
+System.out.println("FREQUENCY OF COMPOSIT MAGIC INTEGERS : "+f);
+        }
+        else
+System.out.println("INVALID INPUT");
+    }
+}
+
