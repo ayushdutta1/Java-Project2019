30f08932f675b260913c5bb1a5e4c7fbf09b79f0
ayushdutta1 <54046142+ayushdutta1@users.noreply.github.com>
2019-09-15 18:22:31 +0530

    Create Question1.java

---
+++ 2014/Question1.java
@@ -0,0 +1,17 @@
+/**
+A Composite Magic number is a positive integer which is composite as well as a magic number.
+
+Composite number:
+A composite number is a number that has more than two factors.
+For example: 10
+Factors are: 1, 2, 5, 10
+
+Magic number:
+A magic number is a number in which the eventual sum of the digits is equal to 1
+For example: 28=2+8=10=1+0=1
+
+Accept two positive integers m and n, where m is less than n as user input. 
+Display the number of Composite magic integers that are in the range between m and n (both inclusive) and
+output them along with the frequency, in the format specified below.
+*/
+
