8d9155518dd749513735f993045d90a976df67d6
ayushdutta1 <54046142+ayushdutta1@users.noreply.github.com>
2019-09-15 18:34:51 +0530

    Update Question1.java

--- 2014/Question1.java
+++ 2014/Question1.java
@@ -18,9 +18,9 @@ output them along with the frequency, in the format specified below.
 import java.util.*;
 public class Composit_Magic
 {
-intisComposit(int a)
+    int isComposit(int a)
     {
-inti,f=0,r=0;
+        int i,f=0,r=0;
         for(i=1;i<=a;i++)
         {
             if(a%i==0)
@@ -29,12 +29,12 @@ inti,f=0,r=0;
         if(f>2)
         r=1;
         return r;
-    }
-intisMagic(int b)
-    {
-intt,r=0,s=0,l;
+   }
+   int isMagic(int b)
+   {
+        int t,r=0,s=0,l;
         t=b;
-for(;t!=0;)
+        for(;t!=0;)
         {
             l=t%10;
             s=s+l;
@@ -52,28 +52,28 @@ for(;t!=0;)
     public static void main()
     {
         Scanner in=new Scanner(System.in);
-Composit_Magicob=new Composit_Magic();
-intm,n,i,f=0;
-System.out.println("Enter Starting Range : ");
+        Composit_Magicob=new Composit_Magic();
+        int m,n,i,f=0;
+        System.out.println("Enter Starting Range : ");
         m=in.nextInt();
-System.out.println("Enter Ending Range : ");
+        System.out.println("Enter Ending Range : ");
         n=in.nextInt();
         if(m<n)
         {
-System.out.println("THE COMPOSIT MAGIC INTEGERS ARE : ");
+            System.out.println("THE COMPOSIT MAGIC INTEGERS ARE : ");
             for(i=m;i<=n;i++)
             {
                 if((ob.isComposit(i)==1)&&(ob.isMagic(i)==1))
                 {
-System.out.print(i+" ");
+                    System.out.print(i+" ");
                     f++;
                 }
             }
-System.out.println();
-System.out.println("FREQUENCY OF COMPOSIT MAGIC INTEGERS : "+f);
+            System.out.println();
+            System.out.println("FREQUENCY OF COMPOSIT MAGIC INTEGERS : "+f);
         }
         else
-System.out.println("INVALID INPUT");
+            System.out.println("INVALID INPUT");
     }
 }
 
